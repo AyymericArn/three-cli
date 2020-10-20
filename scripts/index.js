2d6f92397f7c82e1bca4236ba4bb1677694510cb
Lissandre <lissandre.pasdeloup@hetic.net>
2020-10-20 22:03:23 +0200

    :init: Create a first command

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,28 @@
-const { program } = require('commander')
+const commander = require('commander')
+const fs = require('fs')
+
+const program = commander.createCommand()
 program.version('0.0.1')
 
-program.parse(process.argv)
+program.createCommand = (name) => {
+  const cmd = commander.createCommand(name);
+  return cmd
+}
 
 program
     .command('create <name>')
-    .description('crete a new three.js project')
+    .description('Create a new three.js project')
     .action((name) => {
-        console.log('name :', name);
-    });
+        console.log('Creating your project: ', name)
+        createProjectFolder(name)
+    })
+
+function createProjectFolder(name) {
+    fs.mkdir(`${name}`, { recursive: false }, (err) => {
+      if (err) {
+        err.code === 'EEXIST' ? console.log('A folder with this name already exist.') : console.log(err)
+      }
+    })
+}
+
+program.parse()
